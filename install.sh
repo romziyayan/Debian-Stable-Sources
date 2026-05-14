396fc3804aafcd91a4025954691e5170ec38570a
Romzi Yayan <40373543+romziyayan@users.noreply.github.com>
2026-05-14 08:41:02 +0700

    Enhance install script for web server setup
    Added installation of Apache, MariaDB, and PHP with necessary configurations.

--- install.sh
+++ install.sh
@@ -31,11 +31,18 @@ Components: contrib main non-free-firmware non-free
 Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg" | sudo tee /etc/apt/sources.list.d/stable.sources
 sudo apt update
 curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-kN3i1Mi9fK11CNTRL-2Mv6Xsm5mWGazpobf3gfVGcRB1hRGtvnU
-sudo apt install htop btop git curl wget fastfetch screen tmux tasksel -y
 swapFile="/swap"
 sudo dd if=/dev/zero of=$swapFile bs=1M count=1025
 sudo chmod 600 $swapFile
 sudo mkswap -f $swapFile
 sudo swapon $swapFile
 echo "$swapFile swap swap defaults 0 0" | sudo tee -a /etc/fstab
+sudo apt install htop btop git curl wget fastfetch screen tmux tasksel -y
+sudo apt install apache2 apache2-doc mariadb-server mariadb-client php libapache2-mod-php php-mysql lsb-release -y
+sudo a2enmod rewrite
+sudo a2enmod headers
+sudo a2enmod expires
+sudo systemctl restart apache2
+echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/info.php
+sudo tailscale funnel --bg 80
 sudo poweroff
