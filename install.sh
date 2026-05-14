3ec23ea42a975096ecc5c1412f1a4c0f1a42610c
Romzi Yayan <40373543+romziyayan@users.noreply.github.com>
2026-05-14 08:24:24 +0700

    Create install script for Debian setup and configuration
    This script sets up Debian APT sources, installs necessary packages, creates a swap file, and powers off the system.

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+clear
+sudo mv /etc/apt/sources.list /etc/apt/sources.list~
+sudo mv /etc/apt/sources.list.d/debian.sources /etc/apt/sources.list.d/debian.sources~
+echo "#Debian
+Types: deb deb-src
+URIs: https://deb.debian.org/debian/
+Suites: stable-backports-sloppy stable-backports stable-updates stable
+Components: contrib main non-free-firmware non-free
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+#Debian Security
+Types: deb deb-src
+URIs: https://deb.debian.org/debian-security/
+Suites: stable-security
+Components: contrib main non-free-firmware non-free
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+#Debian Debug
+Types: deb deb-src
+URIs: https://deb.debian.org/debian-debug/
+Suites: stable-backports-debug stable-backports-sloppy-debug stable-debug stable-proposed-updates-debug
+Components: contrib main non-free-firmware non-free
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+#Debian Security Debug
+Types: deb deb-src
+URIs: https://deb.debian.org/debian-security-debug/
+Suites: stable-security-debug
+Components: contrib main non-free-firmware non-free
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg" | sudo tee /etc/apt/sources.list.d/stable.sources
+sudo apt update
+curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-kN3i1Mi9fK11CNTRL-2Mv6Xsm5mWGazpobf3gfVGcRB1hRGtvnU
+sudo apt install htop btop git curl wget fastfetch screen tmux tasksel -y
+swapFile="/swap"
+sudo dd if=/dev/zero of=$swapFile bs=1M count=1025
+sudo chmod 600 $swapFile
+sudo mkswap -f $swapFile
+sudo swapon $swapFile
+echo "$swapFile swap swap defaults 0 0" | sudo tee -a /etc/fstab
+sudo poweroff
