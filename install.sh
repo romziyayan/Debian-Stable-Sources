ef3c131e7777a0f274fbf71c72a539ba154f392c
Romzi Yayan <40373543+romziyayan@users.noreply.github.com>
2026-05-22 08:10:54 +0700

    Update locale configuration in install.sh

--- install.sh
+++ install.sh
@@ -47,8 +47,7 @@ sudo timedatectl set-timezone Asia/Jakarta
 echo "LANG=en_US.UTF-8
 LANGUAGE=en_US.UTF-8
 LC_ALL=id_ID.UTF-8" | sudo tee /etc/locale.conf
-echo "C.UTF-8 UTF-8
-en_US.UTF-8 UTF-8
-id_ID.UTF-8 UTF-8" | sudo tee /etc/locale.gen
+echo "en_US.UTF-8 UTF-8
+id_ID.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
 sudo locale-gen
 sudo tailscale funnel --bg 80
