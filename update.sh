3cdd60d2039b8b7e320e9f7cbb6a5c7fd958f3c7
Romzi Yayan <40373543+romziyayan@users.noreply.github.com>
2026-06-22 20:32:21 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DB_ROOT_PASS=$(openssl rand -base64 16)
+mariadb-secure-installation <<EOF
+$DB_ROOT_PASS
+y
+y
+y
+y
+EOF
+echo "MariaDB root password: $DB_ROOT_PASS" > /root/db_credentials.txt
+chmod 600 /root/db_credentials.txt
