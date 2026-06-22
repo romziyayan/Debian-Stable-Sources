13fb109af7e60d3d6e5e12e07cdf05c717b5e0cf
Romzi Yayan <40373543+romziyayan@users.noreply.github.com>
2026-06-22 20:52:38 +0700

    Refactor update.sh to run as sudo

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo bash -c '
 DB_ROOT_PASS=$(openssl rand -base64 16)
 mariadb-secure-installation <<EOF
 $DB_ROOT_PASS
@@ -8,4 +9,4 @@ y
 y
 EOF
 echo "MariaDB root password: $DB_ROOT_PASS" > /root/db_credentials.txt
-chmod 600 /root/db_credentials.txt
+chmod 600 /root/db_credentials.txt'
